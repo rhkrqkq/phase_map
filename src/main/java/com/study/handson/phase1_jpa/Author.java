a7fd07acb40f1ce5f68bb2ed3d80e28f35350cc2
rhkrqkq <rhkrqkq1025@gmail.com>
2026-09-24 17:53:57 +0900

    test: N+1 check

--- src/main/java/com/study/handson/phase1_jpa/Author.java
+++ src/main/java/com/study/handson/phase1_jpa/Author.java
@@ -3,15 +3,20 @@ package com.study.handson.phase1_jpa;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class Author {
@@ -24,7 +29,7 @@ public class Author {
 	// FK (author_id)는 Book 테이블에만 존재하게 됨.
 	// JPA에서 양방향 맺으면, Author에서도 List<Book> books 를 들고 있어야 autho.getBooks()가 가능해짐.
 	// 여기서 mappedBy="author" 붙여서 이 매핑은 Book엔티이의 author 필드가 담당한다고 명시
-	@OneToMany(mappedBy = "author")
+	@OneToMany(mappedBy = "author", cascade = CascadeType.PERSIST)
 	private List<Book> books = new ArrayList<>();
 
 	public void addBook(Book book) {
--- src/test/java/com/study/handson/DataInitTest.java
+++ src/test/java/com/study/handson/DataInitTest.java
@@ -1,26 +1,47 @@
 package com.study.handson;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.study.handson.phase1_jpa.Author;
 import com.study.handson.phase1_jpa.AuthorRepository;
 import com.study.handson.phase1_jpa.Book;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 
 @SpringBootTest
 class DataInitTest {
 	@Autowired
 	AuthorRepository authorRepository;
 
+	@PersistenceContext
+	EntityManager em;
+
 	@Test
-	void 데이터_생성() {
+	@Transactional
+	void 데이터_생성_조회() {
 		for (int i=0; i<5; i++) {
 			Author author = new Author("작가" + i);
 			for (int j=0; j<3; j++) {
 				author.addBook(new Book("작가 " + i + "의 책" + j));
 			}
 			authorRepository.save(author);
+		}
+
+		em.flush();
+		em.clear();
+
+		List<Author> authors = authorRepository.findAll();
+
+		for (int i=0; i<authors.size(); i++) {
+			Author author = authors.get(i);
+			System.out.println(author.getName() + "의 책" + author.getBooks());
 		}
 	}
 }
